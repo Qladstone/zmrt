fd93b774f6a8f1b7949fefe4395283e0578a38b1
Loo Quanxiang <looquanxiang@gmail.com>
2021-02-23 21:59:26 +0800

    [ZMRT-1] Create the base application using Initializr
    This creates a basic Spring Boot web application that exposes a simple
    "GET /" endpoint which checks if the application is running.

---
+++ src/main/java/com/zendesk/zmrt/ZmrtApplication.java
@@ -0,0 +1,13 @@
+package com.zendesk.zmrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmrtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zendesk/zmrt/ZmrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zendesk.zmrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
